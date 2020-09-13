bfcc47e0ed399dc5f47dbd874b5592227b5526ac
Johan <johanwiren1@gmail.com>
2020-09-13 19:03:39 +0200

    added informative annotation for functional interface

--- src/main/java/database/StatementHandler.java
+++ src/main/java/database/StatementHandler.java
@@ -3,6 +3,7 @@ package database;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface StatementHandler {
   void handler(PreparedStatement statement) throws SQLException;
 }
